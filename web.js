2553c0a98b01426cf5e26dc500b9595a2c31bce7
Wei Lu <luwei.blues@gmail.com>
2014-01-11 14:10:01 +0000

    Changed web.js

--- web.js
+++ web.js
@@ -1,12 +1,30 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var fs = require('fs');
+    var content = fs.readFileSync('index.html');
+    //response.send(content.toString('utf-8'));
+    response.send('hellooo');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+/*
+var express = require('express');
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    var fs = require('fs');
+    var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
+    response.send(buf.toString());
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+*/
